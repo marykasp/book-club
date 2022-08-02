442553a998ea8f5b9ac110923930409c0cd45935
Mary Kasparian <kasparian.mary@gmail.com>
2022-08-02 08:30:44 -0500

    Download packages and fetch practice API data

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+
+const App = () => {
+  const [books, setBooks] = useState([])
+
+  // fetch data - used for creating side effects, schedules events to happen after component is rendered to DOM
+  // useEffect function only needs to run once to fetch data so second parameter does not need to be based on state updates
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://book-club-json.herokuapp.com/books')
+        if (response.ok) {
+          const books = await response.json()
+          console.log('json-ified data', books)
+          // update the books state - save the fetch data
+          setBooks(books)
+        }
+      } catch (errors) {
+        console.log(errors)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return <div>Hello world</div>
+}
+
+export default App
