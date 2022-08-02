ae3e57c50740286342b06817ea9f98cad24933ec
Mary Kasparian <kasparian.mary@gmail.com>
2022-08-02 10:02:51 -0500

    Style and create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import BooksContainer from './components/BooksContainer'
+import Header from './components/Header'
+import {GlobalStyle} from './styles'
 
 const App = () => {
   const [books, setBooks] = useState([])
@@ -24,7 +26,13 @@ const App = () => {
     fetchData()
   }, [])
 
-  return <BooksContainer books={books} />
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <BooksContainer books={books} />
+    </>
+  )
 }
 
 export default App
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Work Sans", sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+  }
+`
